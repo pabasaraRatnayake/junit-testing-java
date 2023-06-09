b56734b3991c0aa18b98ab4fbbe2bcf10b656a66
Pabasara <pabasara.rathnayake@goodhope.co>
2023-06-09 15:41:12 +0530

    Initial Commit

---
+++ java-juint-testing/src/test/java/com/example/javajuinttesting/JavaJuintTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javajuinttesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJuintTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
